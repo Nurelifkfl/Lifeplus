b28de38965a4905635735cf6aa45b0a81243b1ea
Nurelif2708 <ehk2708@gmail.com>
2021-06-05 00:08:31 +0100

    some changes

--- src/test/java/com/lifeplus/pages/BasePage.java
+++ src/test/java/com/lifeplus/pages/BasePage.java
@@ -1,4 +1,12 @@
 package com.lifeplus.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class BasePage {
+
+    @FindBy(xpath = "//*[text()='Log in or register']")
+    WebElement loginButton;
+
+
 }
